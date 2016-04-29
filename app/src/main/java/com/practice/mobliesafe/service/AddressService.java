2ccdc6f74b4ed29635a45b39fc537d4d770380e8
luoxiaojian <1406221797@qq.com>
2016-04-29 10:52:08 +0800

    1.1.9.1 无大改动

--- app/src/main/java/com/practice/mobliesafe/service/AddressService.java
+++ app/src/main/java/com/practice/mobliesafe/service/AddressService.java
@@ -6,11 +6,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -31,6 +35,12 @@ public class AddressService extends Service {
     private WindowManager mWM;
     private View view;
     private SharedPreferences mPref;
+
+    private int startX;
+    private int startY;
+    private WindowManager.LayoutParams params;
+    private int winWidth;
+    private int winHeight;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -109,20 +119,32 @@ public class AddressService extends Service {
     }
 
     /**
-     * 自定义归属地浮窗
+     * 自定义归属地浮窗 需要权限android.permission.SYSTEM_ALERT_WINDOW
      */
     private void showToast(String text) {
         mWM = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+        // 获取屏幕宽高
+        winWidth = mWM.getDefaultDisplay().getWidth();
+        winHeight = mWM.getDefaultDisplay().getHeight();
+
+        params = new WindowManager.LayoutParams();
         params.height = WindowManager.LayoutParams.WRAP_CONTENT;
         params.width = WindowManager.LayoutParams.WRAP_CONTENT;
         params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-                | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
                 | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
         params.format = PixelFormat.TRANSLUCENT;
-        params.type = WindowManager.LayoutParams.TYPE_TOAST;
+        params.type = WindowManager.LayoutParams.TYPE_PHONE;// 电话窗口。它用于电话交互（特别是呼入）。它置于所有应用程序之上，状态栏之下。
+        params.gravity = Gravity.LEFT + Gravity.TOP;// 将重心位置设置为左上方,
+        // 也就是(0,0)从左上方开始,而不是默认的重心位置
         params.setTitle("Toast");
+
+        int lastX = mPref.getInt("lastX", 0);
+        int lastY = mPref.getInt("lastY", 0);
+
+        // 设置浮窗的位置, 基于左上方的偏移量
+        params.x = lastX;
+        params.y = lastY;
 
         // view = new TextView(this);
         view = View.inflate(this, R.layout.toast_address, null);
@@ -138,6 +160,69 @@ public class AddressService extends Service {
         tvText.setText(text);
 
         mWM.addView(view, params);// 将view添加在屏幕上(Window)
+
+        view.setOnTouchListener(new OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // 初始化起点坐标
+                        startX = (int) event.getRawX();
+                        startY = (int) event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        int endX = (int) event.getRawX();
+                        int endY = (int) event.getRawY();
+
+                        // 计算移动偏移量
+                        int dx = endX - startX;
+                        int dy = endY - startY;
+
+                        // 更新浮窗位置
+                        params.x += dx;
+                        params.y += dy;
+
+                        // 防止坐标偏离屏幕
+                        if (params.x < 0) {
+                            params.x = 0;
+                        }
+
+                        if (params.y < 0) {
+                            params.y = 0;
+                        }
+
+                        // 防止坐标偏离屏幕
+                        if (params.x > winWidth - view.getWidth()) {
+                            params.x = winWidth - view.getWidth();
+                        }
+
+                        if (params.y > winHeight - view.getHeight()) {
+                            params.y = winHeight - view.getHeight();
+                        }
+
+                        // System.out.println("x:" + params.x + ";y:" + params.y);
+
+                        mWM.updateViewLayout(view, params);
+
+                        // 重新初始化起点坐标
+                        startX = (int) event.getRawX();
+                        startY = (int) event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // 记录坐标点
+                        Editor edit = mPref.edit();
+                        edit.putInt("lastX", params.x);
+                        edit.putInt("lastY", params.y);
+                        edit.commit();
+                        break;
+
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
 }
